306970e0a82057c14e3ded059d0e5c6a7d847598
DESKTOP-HQ1F2E3\User <phuongnd11@gmail.com>
2021-02-21 09:21:13 -0800

    Add stats for backward moves

--- src/main/java/com/inspireon/chessanalyzer/application/service/ReportService.java
+++ src/main/java/com/inspireon/chessanalyzer/application/service/ReportService.java
@@ -3,6 +3,8 @@ package com.inspireon.chessanalyzer.application.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.inspireon.chessanalyzer.common.enums.ChessSite;
+import com.inspireon.chessanalyzer.domain.cache.PlayerStatCache;
 import com.inspireon.chessanalyzer.web.dtos.OpeningStyle;
 import com.inspireon.chessanalyzer.web.dtos.PlayerOverview;
 import com.inspireon.chessanalyzer.web.dtos.PlayerOverview.PlayerOverviewBuilder;
@@ -13,11 +15,14 @@ public class ReportService {
   @Autowired
   private StyleAnalyzerService styleAnalyzerService;
   
+  @Autowired
+  private PlayerStatCache playerStatCache;
+  
   public PlayerOverview getPlayerOverview(String playerUsername) throws Exception {
     PlayerOverviewBuilder builder = PlayerOverview.builder();
     OpeningStyle openingStyle = styleAnalyzerService.analyzeOpeningStyle(playerUsername);
     String style = styleAnalyzerService.describeOpeningStyle(openingStyle);
     
-    return builder.style(style).build();
+    return builder.style(style).numberOfBackwardMoves(playerStatCache.getBackwardMoves().get(playerUsername + "-" + ChessSite.CHESS_COM.getName())).build();
   }
 }
