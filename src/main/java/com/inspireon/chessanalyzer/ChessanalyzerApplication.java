4247975ed3c787f375c7025885f563c7cde8319e
DESKTOP-HQ1F2E3\User <phuongnd11@gmail.com>
2021-03-14 16:00:59 -0700

    allow cross origins

--- src/main/java/com/inspireon/chessanalyzer/ChessanalyzerApplication.java
+++ src/main/java/com/inspireon/chessanalyzer/ChessanalyzerApplication.java
@@ -27,7 +27,7 @@ public class ChessanalyzerApplication extends SpringBootServletInitializer {
       return new WebMvcConfigurer() {
           @Override
           public void addCorsMappings(CorsRegistry registry) {
-              registry.addMapping("/**").allowedOrigins("chessstats.io");
+              registry.addMapping("/**").allowedOrigins("http://chessstats.io");
           }
       };
   }
