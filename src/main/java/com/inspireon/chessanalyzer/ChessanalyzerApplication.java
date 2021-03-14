5d0609b1f92f6c53baaa5f8eea2202d9fcae88e7
DESKTOP-HQ1F2E3\User <phuongnd11@gmail.com>
2021-03-14 15:50:08 -0700

    Add cors mapping

--- src/main/java/com/inspireon/chessanalyzer/ChessanalyzerApplication.java
+++ src/main/java/com/inspireon/chessanalyzer/ChessanalyzerApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan
@@ -17,5 +20,15 @@ public class ChessanalyzerApplication extends SpringBootServletInitializer {
     
   public static void main(String[] args) {
     SpringApplication.run(ChessanalyzerApplication.class, args);
+  }
+  
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+          @Override
+          public void addCorsMappings(CorsRegistry registry) {
+              registry.addMapping("/**");
+          }
+      };
   }
 }
